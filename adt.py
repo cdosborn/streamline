95319438ea7c648ac91c3e57d37cc8d8308da62e
Connor Osborn <cdosborn@uw.edu>
2013-06-03 22:37:37 -0700

    fixed indentation

--- adt.py
+++ adt.py
@@ -26,21 +26,21 @@ class Tree:
     def __init__(self, root=None):
         self.root = root
 
-def printRoot(self):
-    self.root.printNode()
+    def printRoot(self):
+        self.root.printNode()
 
-def write(self, format=None, filter=None):
-    return self._explore(self.root)
+    def write(self, format=None, filter=None):
+        return self._explore(self.root)
 
-def _explore(self, node, format=None, filter=None): # test at the bottom
-    cumhtml = "<" + node.tag + ">"
-    if filter:
-        node.children = filter(node.children)
-    for child in node.children:
-        if child.tag == "text":
-            cumhtml += child.value
-        elif format:
-            cumhtml += format(self._explore(child))
-        else:
-            cumhtml += self._explore(child)
-    return cumhtml + "</" + node.tag + ">"
+    def _explore(self, node, format=None, filter=None): # test at the bottom
+        cumhtml = "<" + node.tag + ">"
+        if filter:
+            node.children = filter(node.children)
+        for child in node.children:
+            if child.tag == "text":
+                cumhtml += child.value
+            elif format:
+                cumhtml += format(self._explore(child))
+            else:
+                cumhtml += self._explore(child)
+        return cumhtml + "</" + node.tag + ">"
