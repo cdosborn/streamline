a861a303f412e6987f8279e463438144fc51e2a8
scascketta <scascketta@gmail.com>
2013-06-05 15:55:46 -0500

    Added better exception handling, started integrating new parse files.

--- streamline.py
+++ streamline.py
@@ -1,104 +1,32 @@
-from urllib2 import urlopen
-
-'''Sends a request for the HTML document at "address", 
-stores the file in rawPage, and then reads it into a 
-raw HTML document in rawHTML.'''
-
-try:
-  address = raw_input("Enter a valid URL: ")
-  rawPage = urlopen(address)
-  rawHTML = rawPage.read()
-  htmlDict = {}
-except:
-  print "Request could not be completed, the site probably doesn't allow for scraping."
-
-'''Parses a raw HTML string (rawHTML) for all elements 
-matching the HTML tag (tagText) and 
-stores all instances of that tag in a dictionary.'''
-def parseHtmlTags(rawHTML, tagText, htmlDict):
-  startTag = "<" + tagText + ">"
-  endTag = "</" + tagText + ">"
-
-  startIndex = 0
-  endIndex = 0
-
-  dictItemNum = 0
-  htmlDict[tagText] = ""
-  itemIndex = tagText + str(dictItemNum)
-  itemDict = {}
-
-  while startIndex != -1 and endIndex != -1:
-
-    startIndex = rawHTML.find(startTag, startIndex + 1)
-    endIndex = rawHTML.find(endTag, endIndex + 1)
-
-    if startIndex == -1 and endIndex == -1:
-      break
-
-    if startIndex > endIndex:
-      while startIndex > endIndex:
-        endIndex = rawHTML.find(endTag, endIndex + 1)
-
-    snippet = rawHTML[startIndex + len(startTag):endIndex]
-
-    if isArticleText(snippet):
-      itemDict[itemIndex] = snippet
-      dictItemNum += 1
-      itemIndex = tagText + str(dictItemNum)
-
-  htmlDict[tagText] = itemDict
-
-def isArticleText(inputHtml):
-  forbidden = ["<form", "<input", "DOCTYPE", "<span"]
-  for item in forbidden:
-    if inputHtml.find(item) != -1:
-      return False
-  return True
-
-'''Creates a barebones HTML document and adds the 
-extracted elements contained in htmlDict with the 
-necessary HTML formatting in between.'''
-
-def makeHTML(htmlDict, address):
-  htmlFile = open("streamlined.html", "w")
-  htmlFile.writelines(
-"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link href='http://fonts.googleapis.com/css?family=Oswald:400,700|Open+Sans' rel='stylesheet' type='text/css'>
-<link href='streamline.css' rel='stylesheet'>
-<meta charset="utf-8">
-"""
-    )
-  htmlFile.writelines("<title>" + htmlDict["title"]["title0"] + "</title>")
-
-  htmlFile.writelines(
-"""
-</head>
-<body>
-"""
-    )
-  htmlFile.writelines("<div class='container'>")
-  htmlFile.writelines("<h1>" + htmlDict["title"]["title0"] + "</h1>")
-  htmlFile.writelines("<br><h3><a href='" + address + "'>Link to the original article</a></h3><br>")
-
-  numPItems = len(htmlDict["p"])
-  for item in range(0, numPItems):
-    keystr = "p" + str(item)
-    htmlFile.writelines("<br><p>" + htmlDict["p"][keystr] + "</p>")
-
-  htmlFile.writelines(
-"""
-</div>
-</body>
-</html>
-"""
-    )
-  htmlFile.close()
+import urllib2
+from urllib2 import urlopen, URLError, HTTPError
+import parse, adt
 
 
+invalidURL = True
 
-parseHtmlTags(rawHTML,"title", htmlDict)
-parseHtmlTags(rawHTML,"p", htmlDict)
-makeHTML(htmlDict, address)
+while invalidURL:
+    try:
+        address = raw_input("Enter a valid URL: ")
+        rawPage = urlopen(address)
+        rawHTML = rawPage.read()
+        # rawHTML = "<html><body><p>asdfklioet</p></body></html>"
+        invalidURL = False
+
+    except urllib2.URLError: # Usually indicative of an HTTP 403 error (request denied by server as per robots.txt)
+        print "The website linked to does not allow for web crawling. Please try a different website."
+
+    except urllib2.HTTPError: # Unusual error, tends to happen when URL causes a non-GET HTTP request.
+        print "The link causes a nonstandard HTTP request."
+
+    except ValueError: # Invalid URL form
+        print "You did not enter a valid URL. It should be in the form 'http://www.google.com'."
+
+    except Exception: # Generic error, DOES NOT catch KeyboardInterrupt, SystemExit
+        print "A generic error occurred."
+
+def parseWrapper(html, filters=None):
+    tree = adt.Tree(parse.parse(html))
+    return tree.write(filters)
+
+# print parseWrapper(rawHTML)
